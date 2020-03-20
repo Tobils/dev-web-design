7b83a2caf04f21f24006c8c8cf2487acb0426acc
dev-tobil <ade.suhada@mail.ugm.ac.id>
2020-03-20 08:05:13 +0700

    team section

--- SOLO/js/script.js
+++ SOLO/js/script.js
@@ -5,3 +5,19 @@ $(window).on('load', function() { // make sure to load whole site
     $('#status').fadeOut();
     $('#preloader').delay(350).fadeOut();
 });
+
+/* ==========================================
+                Team 
+========================================== */
+$(function() {
+    $("#team-members").owlCarousel({
+        items: 2,
+        autoplay: true,
+        smartSpeed: 700,
+        loop: true,
+        autoplayHoverPause: true,
+        nav: true,
+        navText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>'],
+        dots: false
+    });
+});
